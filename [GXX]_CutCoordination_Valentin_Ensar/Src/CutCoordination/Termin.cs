342a7c38613158c38f72913a2fee8509830c79bd
Ensar Bugday <bilal-ensar.bugday@student.htl-rankweil.at>
2024-06-11 11:25:55 +0200

    termin class erstellt

---
+++ [GXX]_CutCoordination_Valentin_Ensar/Src/CutCoordination/Termin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G06_DBI_CutCoordination
+{
+    internal class Termin
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Telefonnummer { get; set; }
+        public string Datum { get; set; }
+        public string Uhrzeit { get; set; }
+        public int Dauer { get; set; }
+    }
+}
---
+++ [GXX]_CutCoordination_Valentin_Ensar/Src/CutCoordination/TerminManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G06_DBI_CutCoordination
+{
+    internal class TerminManager
+    {
+
+
+        public static Termin AddTermin(string vorname, string nachname, string telefonnummer, string datum, string uhrzeit, int dauer)
+        {
+            Termin newTermin = new Termin
+            {
+                Vorname = vorname,
+                Nachname = nachname,
+                Telefonnummer = telefonnummer,
+                Datum = datum,
+                Uhrzeit = uhrzeit,
+                Dauer = dauer
+            };
+            return newTermin;
+
+        }
+            
+            
+        }
+        public static void AddTerminToSql(Termin termin)
+        {
+            //string path = "Data Source=database/friseur.db";
+
+        }
+    }
+}
